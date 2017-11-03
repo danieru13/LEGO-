3eed138e3967ad70d5b3454022e14656fc289902
danieru13 <danieru1313@gmail.com>
2017-11-02 21:04:08 -0500

    Update manual.py

--- manual.py
+++ manual.py
@@ -1,8 +1,8 @@
 from BrickPi import *
 
-import sys, tty, termios, time
+import sys, tty, termios, time #Se importan librearias
 
-#Mover
+#Funcion para recibir caracteres del teclado
 def getch():
 	fd = sys.stdin.fileno()
 	old_settings = termios.tcgetattr(fd)
@@ -12,7 +12,7 @@ def getch():
 	finally:
 	termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 	return ch
-
+#Funciones de movimiento que indican a los motores a que velocidad revolucionar
 def acelerar():
 	#Mover hacia adelante.
 	BrickPi.MotorSpeed[motorDer] = 255
@@ -30,28 +30,34 @@ def izquierda():
 	BrickPi.MotorSpeed[motorDer] = 255
 	BrickPi.MotorSpeed[motorIzq] = -255
 
-BrickPiSetup()
+BrickPiSetup() 
 motorIzq = PORT_C #Puerto motor izquierda
 motorDer = PORT_B #Puerto motor derecho
-BrickPi.MotorEnable[motorDer] = 1
-BrickPi.MotorEnable[motorIzq] = 1
+BrickPi.MotorEnable[motorDer] = 1 #Habilita motor derecho
+BrickPi.MotorEnable[motorIzq] = 1 #Habilita motor izquierdo
 
+#Ciclo para recibir comandos
 while True:
-	char = getch()
+	char = getch() #Recibe comando de teclado
 	if(char != "w" and char != "a" and char != "s" and char != "d" and != "x"):
 		print("Esperando comando")
 		char = getch()
 	else:
+		#Si recibe w acelera
 		if (char == "w"):
 			acelerar()
+		#Si recibe s retrocede
 		if (char == "s"):
 			retroceder()
+		#Si recibe a gira a la izquierda
 		if (char == "a"):
 			izquierda()
+		#Si recibe d gira a la derecha	
 		if (char == "d"):
-			derecha()		
+			derecha()
+		#Si recibe x se rompe el ciclo y termina el codigo	
 		if (char == "x"):
 			break
-BrickPiUpdateValues()
-char = getch()
-time.sleep(.1)				
+BrickPiUpdateValues() #Actualiza valores
+char = getch() #Recibe caracteres
+time.sleep(.1) #Le da una pausa al codigo para que los motores puedan actualizarse			
